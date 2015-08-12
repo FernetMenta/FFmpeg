3dad95d1bd2508e91b0cd3704892258945e6e57a
Rainer Hochecker <fernetmenta@online.de>
2015-08-12 19:12:18 +0200

    more work

--- libavcodec/vaapi_hevc.c
+++ libavcodec/vaapi_hevc.c
@@ -51,18 +51,92 @@ static void init_vaapi_pic(VAPictureHEVC *va_pic)
  *                             supersedes pic's field type if nonzero.
  */
 static void fill_vaapi_pic(VAPictureHEVC *va_pic,
-                           HEVCFrame *pic,
-                           int pic_structure)
+                           HEVCFrame     *pic,
+                           int            pic_structure)
 {
+    pic_structure &= PICT_FRAME; /* PICT_TOP_FIELD|PICT_BOTTOM_FIELD */
 
+    va_pic->picture_id = ff_vaapi_get_surface_id(pic->frame);
+
+    va_pic->flags = 0;
+    if (pic_structure != PICT_FRAME) {
+        va_pic->flags |= VA_PICTURE_HEVC_FIELD_PIC;
+        va_pic->flags |= (pic_structure & PICT_TOP_FIELD) ? 0 : VA_PICTURE_HEVC_BOTTOM_FIELD;
+    }
+    if (pic->flags & HEVC_FRAME_FLAG_LONG_REF)
+        va_pic->flags |= VA_PICTURE_HEVC_LONG_TERM_REFERENCE;
+
+    va_pic->pic_order_cnt = pic->poc;
 }
 
-/** Decoded Picture Buffer (DPB). */
-typedef struct DPB {
-    int            size;        ///< Current number of reference frames in the DPB
-    int            max_size;    ///< Max number of reference frames. This is FF_ARRAY_ELEMS(VAPictureParameterBufferHEVC.ReferenceFrames)
-    VAPictureHEVC *va_pics;     ///< Pointer to VAPictureParameterBufferHEVC.ReferenceFrames array
-} DPB;
+static void fill_picture_parameters(const HEVCContext *h,
+                                    VAPictureParameterBufferHEVC *pp)
+{
+    const HEVCFrame *current_picture = h->ref;
+    int i, j;
+
+    memset(pp, 0, sizeof(*pp));
+
+    fill_vaapi_pic(&pp->CurrPic, current_picture, h->picture_struct);
+
+    pp->pic_width_in_luma_samples  = h->sps->min_cb_width;
+    pp->pic_height_in_luma_samples = h->sps->min_cb_height;
+
+    pp->pic_fields.bits.chroma_format_idc = h->sps->chroma_format_idc;
+
+    pp->sps_max_dec_pic_buffering_minus1         = h->sps->temporal_layer[h->sps->max_sub_layers - 1].max_dec_pic_buffering - 1;
+    pp->log2_min_luma_coding_block_size_minus3   = h->sps->log2_min_cb_size - 3;
+    pp->log2_diff_max_min_luma_coding_block_size = h->sps->log2_diff_max_min_coding_block_size;
+    pp->log2_min_transform_block_size_minus2     = h->sps->log2_min_tb_size - 2;
+    pp->log2_diff_max_min_transform_block_size   = h->sps->log2_max_trafo_size  - h->sps->log2_min_tb_size;
+    pp->max_transform_hierarchy_depth_inter      = h->sps->max_transform_hierarchy_depth_inter;
+    pp->max_transform_hierarchy_depth_intra      = h->sps->max_transform_hierarchy_depth_intra;
+    pp->num_short_term_ref_pic_sets              = h->sps->nb_st_rps;
+//    pp->num_long_term_ref_pics_sps               = h->sps->num_long_term_ref_pics_sps;
+
+    pp->num_ref_idx_l0_default_active_minus1     = h->pps->num_ref_idx_l0_default_active - 1;
+    pp->num_ref_idx_l1_default_active_minus1     = h->pps->num_ref_idx_l1_default_active - 1;
+    pp->init_qp_minus26                          = h->pps->pic_init_qp_minus26;
+
+    pp->pps_cb_qp_offset            = h->pps->cb_qp_offset;
+    pp->pps_cr_qp_offset            = h->pps->cr_qp_offset;
+    if (h->pps->tiles_enabled_flag) {
+        pp->num_tile_columns_minus1 = h->pps->num_tile_columns - 1;
+        pp->num_tile_rows_minus1    = h->pps->num_tile_rows - 1;
+
+        if (!h->pps->uniform_spacing_flag) {
+            for (i = 0; i < h->pps->num_tile_columns; i++)
+                pp->column_width_minus1[i] = h->pps->column_width[i] - 1;
+
+            for (i = 0; i < h->pps->num_tile_rows; i++)
+                pp->row_height_minus1[i] = h->pps->row_height[i] - 1;
+        }
+    }
+
+    pp->diff_cu_qp_delta_depth           = h->pps->diff_cu_qp_delta_depth;
+    pp->pps_beta_offset_div2             = h->pps->beta_offset / 2;
+    pp->pps_tc_offset_div2               = h->pps->tc_offset / 2;
+    pp->log2_parallel_merge_level_minus2 = h->pps->log2_parallel_merge_level - 2;
+//    pp->CurrPicOrderCntVal               = h->poc;
+
+    // fill RefPicList from the DPB
+    for (i = 0, j = 0; i < FF_ARRAY_ELEMS(pp->ReferenceFrames); i++) {
+        const HEVCFrame *frame = NULL;
+        while (!frame && j < FF_ARRAY_ELEMS(h->DPB)) {
+            if (&h->DPB[j] != current_picture && (h->DPB[j].flags & (HEVC_FRAME_FLAG_LONG_REF | HEVC_FRAME_FLAG_SHORT_REF)))
+                frame = &h->DPB[j];
+            j++;
+        }
+
+        if (frame) {
+            fill_vaapi_pic(&pp->ReferenceFrames[i], frame, frame->flags & HEVC_FRAME_FLAG_LONG_REF);
+        } else {
+            pp->ReferenceFrames[i].picture_id = VA_PICTURE_HEVC_INVALID;
+            pp->ReferenceFrames[i].pic_order_cnt = 0;
+        }
+    }
+}
+
 
 /** Initialize and start decoding a frame with VA API. */
 static int vaapi_hevc_start_frame(AVCodecContext          *avctx,
@@ -82,10 +156,7 @@ static int vaapi_hevc_start_frame(AVCodecContext          *avctx,
     pic_param = ff_vaapi_alloc_pic_param(vactx, sizeof(VAPictureParameterBufferHEVC));
     if (!pic_param)
         return -1;
-    fill_vaapi_pic(&pic_param->CurrPic, h->ref, h->picture_struct);
-
-//    if (fill_vaapi_ReferenceFrames(pic_param, h) < 0)
-//        return -1;
+    fill_picture_parameters(h, pic_param);
 
     return 0;
 }
